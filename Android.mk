a8e9e9339c7dcd265d9c084c87b9028a6bf21e47
Ashwin Ramesh <ashwinr64@lineageos.org>
2017-07-23 12:04:40 +0530

    taido: Add VoLTE stack and update a few blobs

--- Android.mk
+++ Android.mk
@@ -29,4 +29,14 @@ LOCAL_DEX_PREOPT := false
 LOCAL_MODULE_SUFFIX := .apk
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := com.google.widevine.software.drm
+LOCAL_MODULE_OWNER := motorola
+LOCAL_SRC_FILES := proprietary/framework/com.google.widevine.software.drm.jar
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_MODULE_SUFFIX := .jar
+include $(BUILD_PREBUILT)
+
 endif
